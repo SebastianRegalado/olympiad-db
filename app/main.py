0172ea8037680bab1f191dc29ab3178eff39a93c
Sebastian Regalado <sdregala@uwaterloo.ca>
2025-07-12 17:04:42 -0400

    list problems in main page

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
+from sqlalchemy.orm import Session
+
 from .routes import router
+from .db import get_db
+from .models import Problem
 
 app = FastAPI()
 app.include_router(router)
@@ -10,5 +14,8 @@ templates = Jinja2Templates(directory="app/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def root(request: Request, db: Session = Depends(get_db)):
+    problems = db.query(Problem).order_by(Problem.id.desc()).all()
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "problems": problems}
+    )
